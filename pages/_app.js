af04119d53e3ee88008418f23c27a760c7278142
samuelberhane <1samibrhane@gmail.com>
2023-01-30 16:55:42 +0300

    Add Spotify next-auth authentication.

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,17 @@
 import "../styles/globals.css";
 import { Provider } from "react-redux";
 import store from "../redux/store";
+import { SessionProvider } from "next-auth/react";
 
-export default function App({ Component, pageProps }) {
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}) {
   return (
-    <Provider store={store}>
-      <Component {...pageProps} />
-    </Provider>
+    <SessionProvider session={session}>
+      <Provider store={store}>
+        <Component {...pageProps} />
+      </Provider>
+    </SessionProvider>
   );
 }
