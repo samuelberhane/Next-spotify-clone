d2b33706505ceb29ecdb46bd26c847e628f25293
samuelberhane <1samibrhane@gmail.com>
2023-02-01 16:55:29 +0300

    Next and Prev song functionality with and without shuffle.

--- components/Player.jsx
+++ components/Player.jsx
@@ -8,30 +8,43 @@ import Link from "next/link";
 import { useRef } from "react";
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
-import { RESTART_SONG, PAUSE_SONG } from "../redux/slice/songSlice";
+import {
+  RESTART_SONG,
+  PAUSE_SONG,
+  selectShuffle,
+  UPDATE_SHUFFLE,
+  selectCurrentSongs,
+  selectSongIndex,
+  PREV_SONG,
+  NEXT_SONG,
+  SHUFFLE_SONG,
+} from "../redux/slice/songSlice";
 
 const Player = () => {
   const isPlaying = useSelector(selectIsPlaying);
   const activeSong = useSelector(selectActiveSong);
+  const shuffle = useSelector(selectShuffle);
+  const currentSongs = useSelector(selectCurrentSongs);
+  const songIndex = useSelector(selectSongIndex);
   const dispatch = useDispatch();
-  console.log("isPlaying", isPlaying);
-  console.log("activeSong", activeSong);
   const ref = useRef(null);
+
+  console.log("currentSongs", currentSongs);
+  console.log("songIndex", songIndex);
 
   useEffect(() => {
     if (isPlaying) {
-      console.log(ref.current);
       ref.current.play();
     }
     if (!isPlaying) {
       ref?.current.pause();
     }
-  }, [isPlaying]);
+  }, [isPlaying, activeSong]);
 
   return (
     <div className="bg-[rgb(20,20,22)] z-50 sm:left-24 md:left-56 fixed bottom-0 left-0 right-0 text-white">
       <div className="flex item-center py-4 px-1 sm:px-8 justify-between">
-        <div className="hidden lg:flex items-center gap-2">
+        <div className="hidden lg:flex items-center gap-2 w-[300px]">
           <img
             src={
               activeSong?.images?.coverart ||
@@ -59,16 +72,21 @@ const Player = () => {
               }
               className="mt-1 text-sm text-gray-300"
             >
-              {activeSong?.share?.subject?.length < 40
+              {activeSong?.share?.subject?.length < 35
                 ? activeSong?.share?.subject
-                : activeSong?.share?.subject?.slice(0, 38) + "..."}
+                : activeSong?.share?.subject?.slice(0, 30) + "..."}
             </Link>
           </div>
         </div>
         <div>
           <div className="flex items-center gap-4 justify-center text-xl mb-2">
             <MdOutlineRestartAlt className="cursor-pointer" />
-            <BiSkipPrevious className="cursor-pointer" />
+            <BiSkipPrevious
+              className="cursor-pointer"
+              onClick={() => {
+                !shuffle ? dispatch(PREV_SONG()) : dispatch(SHUFFLE_SONG());
+              }}
+            />
             {isPlaying ? (
               <BsFillPauseFill
                 className="text-2xl cursor-pointer"
@@ -81,8 +99,16 @@ const Player = () => {
               />
             )}
 
-            <BiSkipNext className="cursor-pointer" />
-            <BiShuffle className="cursor-pointer" />
+            <BiSkipNext
+              className="cursor-pointer"
+              onClick={() => {
+                !shuffle ? dispatch(NEXT_SONG()) : dispatch(SHUFFLE_SONG());
+              }}
+            />
+            <BiShuffle
+              className={`cursor-pointer ${shuffle && "text-green-400"}`}
+              onClick={() => dispatch(UPDATE_SHUFFLE())}
+            />
           </div>
           <div className="flex items-center gap-1">
             <p className="text-sm">0:00</p>
@@ -95,7 +121,15 @@ const Player = () => {
           <input type="range" name="" id="" className="mb-2" />
         </div>
       </div>
-      <audio src={activeSong?.hub?.actions[1]?.uri} ref={ref} loop={true} />
+      <audio
+        src={
+          activeSong?.hub?.actions
+            ? activeSong?.hub?.actions[1]?.uri
+            : `/audio/defaultAudio.mp3`
+        }
+        ref={ref}
+        loop={true}
+      />
     </div>
   );
 };
--- redux/slice/songSlice.js
+++ redux/slice/songSlice.js
@@ -4,6 +4,14 @@ const initialState = {
   currentSongs: [],
   isPlaying: false,
   activeSong: {},
+  shuffle: false,
+  songIndex: 0,
+};
+
+const checkIndex = (index, state) => {
+  if (index > state.currentSongs.length - 1) return 0;
+  if (index < 0) return state.currentSongs.length - 1;
+  return index;
 };
 
 const songSlice = createSlice({
@@ -12,7 +20,9 @@ const songSlice = createSlice({
   reducers: {
     PLAY_SONG: (state, action) => {
       state.currentSongs = [...state.currentSongs, action.payload];
-      (state.activeSong = action.payload), (state.isPlaying = true);
+      state.activeSong = action.payload;
+      state.isPlaying = true;
+      state.songIndex = state.songIndex + 1;
     },
     PAUSE_SONG: (state, action) => {
       state.isPlaying = false;
@@ -22,14 +32,45 @@ const songSlice = createSlice({
     },
     FIRST_SONG: (state, action) => {
       state.activeSong = action.payload;
+      state.currentSongs = [action.payload];
+    },
+    UPDATE_SHUFFLE: (state, action) => {
+      state.shuffle = !state.shuffle;
+    },
+    PREV_SONG: (state, action) => {
+      state.isPlaying = true;
+      state.songIndex = checkIndex(state.songIndex - 1, state);
+      state.activeSong = state.currentSongs[state.songIndex];
+    },
+    NEXT_SONG: (state, action) => {
+      state.isPlaying = true;
+      state.songIndex = checkIndex(state.songIndex + 1, state);
+      state.activeSong = state.currentSongs[state.songIndex];
+    },
+    SHUFFLE_SONG: (state, action) => {
+      (state.isPlaying = true),
+        (state.songIndex = Math.floor(
+          Math.random() * state.currentSongs.length
+        ));
+      state.activeSong = state.currentSongs[state.songIndex];
     },
   },
 });
 
-export const { PLAY_SONG, PAUSE_SONG, FIRST_SONG, RESTART_SONG } =
-  songSlice.actions;
+export const {
+  PLAY_SONG,
+  PAUSE_SONG,
+  FIRST_SONG,
+  RESTART_SONG,
+  UPDATE_SHUFFLE,
+  NEXT_SONG,
+  PREV_SONG,
+  SHUFFLE_SONG,
+} = songSlice.actions;
 export const selectCurrentSongs = (state) => state.song.currentSongs;
 export const selectIsPlaying = (state) => state.song.isPlaying;
 export const selectActiveSong = (state) => state.song.activeSong;
+export const selectShuffle = (state) => state.song.shuffle;
+export const selectSongIndex = (state) => state.song.songIndex;
 
 export default songSlice.reducer;
